bf04b980ec99a67f29f8d0e179a5d3758eb393a6
teslim191 <40440655+teslim191@users.noreply.github.com>
2021-09-09 09:08:58 +0100

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -21,7 +21,7 @@ if (!isset($_SESSION["email"])) {
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div class="container">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#"><i class="fab fa-apple-pay fa-3x"></i></a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
